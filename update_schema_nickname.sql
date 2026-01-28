666ccf296a5c95cd4aef2565682e0067fb39a1d4
Strushs <dawidstruzynski1@wp.pl>
2026-01-28 19:21:13 +0100

    last touches

--- update_schema_nickname.sql
+++
@@ -1,8 +0,0 @@
--- Add nickname column to profiles table
-ALTER TABLE public.profiles 
-ADD COLUMN nickname text;
-
--- Optional: Backfill existing nicknames with Study Field or generic name to prevent nulls in UI
-UPDATE public.profiles 
-SET nickname = study_field 
-WHERE nickname IS NULL;
